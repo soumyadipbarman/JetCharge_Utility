9a0acc95cb771ec616361343b4d2b17f2be8ea76
soumyadipbarman <barmansoumyadip@gmail.com>
2022-11-08 19:09:10 +0530

    update

--- lumiweight/madgraph/scalehist_binsamples/merge_run.sh
+++ lumiweight/madgraph/scalehist_binsamples/merge_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 python merge.py
-rm addfile.root
+rm MG5_PY8_bin.root
 rm add.sh
-echo 'hadd addfile.root' > xx.sh
+echo 'hadd MG5_PY8_bin.root' > xx.sh
 ls MG*.root >> xx.sh
 tr '\r\n' ' ' < xx.sh >> add.sh 
 chmod 755 add.sh
--- lumiweight/pythia8/scalehist_binsamples/merge_run.sh
+++ lumiweight/pythia8/scalehist_binsamples/merge_run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 python merge.py
-rm addfile.root
+rm PY8_bin.root
 rm add.sh
-echo 'hadd addfile.root' > xx.sh
+echo 'hadd PY8_bin.root' > xx.sh
 ls PY*.root >> xx.sh
 tr '\r\n' ' ' < xx.sh >> add.sh 
 chmod 755 add.sh
